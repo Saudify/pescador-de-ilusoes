28b4f4e8fba7cc736bdc94c2d5a64bcb5af08dd0
Janderson <jandersonmartins10@gmail.com>
2018-05-01 08:59:37 -0300

    Creating hit function to get a web page

--- src/fisher/index.js
+++ src/fisher/index.js
@@ -1,6 +1,14 @@
 import got from 'got'
+import cheerio from 'cheerio'
 
-export async function hit (url) {
-  // ...
-  const response = await got(url)
+/**
+ * Request a web page.
+ * @param {String} url Page url.
+ * @param {Object} options Request options.
+ * @returns {Function} Page loaded with cheerio.
+ */
+export async function hit (url, options = {}) {
+  const response = await got(url, options)
+  const $ = cheerio.load(response.body)
+  return $
 }
--- test/integration/fisher/index.test.js
+++ test/integration/fisher/index.test.js
@@ -1,7 +1,9 @@
 import test from 'ava'
 import { hit } from '../../../src/fisher'
 
-test('hit: should return loaded page', async t => {
-  const actual = await hit('github.com/Saudify')
-  t.pass()
+test('hit: should return loaded page with cheerio', async t => {
+  const $ = await hit('github.com/Saudify')
+  const actual = $('h1.org-name.lh-condensed').text().trim()
+  const expected = 'Saudify'
+  t.is(actual, expected)
 })
